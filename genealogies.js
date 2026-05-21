2e2fe415d60f87de520833432dd2ddbb6e0b9a8d
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-05-21 15:20:05 +0200

    Create genealogies.js

---
+++ genealogies.js
@@ -0,0 +1,104 @@
+canoniques = {
+
+	"saint": "Saint ",
+	"sainte": "Sainte ",
+
+	"bienheureux": "Bienheureux ",
+	"geseenten": "Geseenten ",
+
+}
+
+ecclesiastiques = {
+
+	"archevêque":  "Archevêque ",
+
+	"évêque":  "Évêque ",
+
+	"cardinal":  "Cardinal ",
+
+	"abbé":  "Abbé ",
+
+	"moine":  "Moine ",
+
+	"prêtre":  "Prêtre ",
+
+	"chanoine":  "Chanoine ",
+
+	"religieux":  "Religieux ",
+
+}
+
+temporels = {
+
+	"empereur":  "Empereur ",
+	"царь": "Царь ",
+	"sultan": "Sultan ",
+
+	"roi": "Roi",
+	"reine": "Reine ",
+
+	"roi.saliens": "Roi des Saliens ",
+	"roi.orléans": "Roi d’Orléans ",
+	"roi.paris": "Roi de Paris ",
+	"roi.austrasie": "Roi d’Austrasie ",
+	"roi.aquitaine": "Roi d’Aquitaine ",
+
+	"fürst": "Fürst ",
+
+	"großherzog": "Großherzog ",
+	"großherzogin": "Großherzogin ",
+	"великая-княжна": "Великая княжна ",
+
+	"duc": "Duc ",
+	"dug":  "Dug ",
+	"herzog": "Herzog ",
+	"duchesse": "Duchesse ",
+	"dugez":  "Dugez ",
+	"herzogin": "Herzogin ",
+
+	"marquis":  "Marquis ",
+	"marquise":  "Marquise ",
+
+	"comte": "Comte ",
+	"comtesse": "Comtesse ",
+
+	"présomptif":  "Héritier présomptif ",
+	"mutmaßliche":  "Mutmaßliche erbe ",
+	"царевич":  "Царевич ",
+
+	"chef": "Chef ",
+	"exclus": "Exclus ",
+	"baillistr": "Baillistr ",
+	"madame-royale": "Madame Royale",
+	"avoué-du-saint-sépulcre": "Avoué du Saint-Sépulcre",
+
+};
+
+function titrer() {
+	document.querySelectorAll('div[class]').forEach(
+		personne => {
+			titres = [];
+			if (personne.classList.contains("roi") && personne.classList.contains("austrasie")) {
+				titres.push("Roi d’Austrasie");
+			} else if (personne.classList.contains("roi") && personne.classList.contains("neustrie")) {
+				titres.push("Roi de Neustrie");
+			} else if (personne.classList.contains("roi") && personne.classList.contains("paris") && personne.classList.contains("orléans")) {
+				titres.push("Roi de Paris puis d’Orléans");
+			} else if (personne.classList.contains("roi") && personne.classList.contains("consort")) {
+				titres.push("Roi consort");
+			} else if (personne.classList.contains("duc") && personne.classList.contains("roi")) {
+				titres.push("Duc puis Roi");
+			} else {
+				for ([classe, nom] of Object.entries(Object.assign(canoniques, ecclesiastiques, temporels))) {
+					if (personne.classList.contains(classe)) {titres.push(nom);}
+				}
+			}
+			if (titres.length > 0) {
+				prefixe = titres.join(" ") + " ";
+				personne.innerHTML = prefixe + personne.innerHTML;
+			}
+		}
+	);
+}
+
+window.onload = titrer;
