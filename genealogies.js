c43bbcd2ed301a149f0c92f42729df8c4935d73f
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-07-25 23:41:55 +0200

    Update genealogies.js

--- genealogies.js
+++ genealogies.js
@@ -15,7 +15,6 @@ function masquer(bouton) {
 	divs = bouton.parentElement.parentElement.querySelectorAll("div");
 	divs.forEach(
 		function(div) {
-			// if (div.style.opacity == "0") {div.style.opacity = "1";} else {div.style.opacity = "0";}
 			if (div.style.display == "none") {div.style.display = "block";} else {div.style.display = "none";}
 		}
 	);
